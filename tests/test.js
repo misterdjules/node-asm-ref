a7219e341d195d66624e9cd02b7f069248452fac
Julien Gilli <jgilli@fastmail.fm>
2014-06-03 11:31:54 -0400

    Node.js binding now performs all sqlite I/Os asynchronously.

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,8 @@
 var x86AsmRef = require('../build/Debug/x86-asm-ref');
 
-var x86Instructions = x86AsmRef.getAllInstructions();
-console.dir(x86Instructions);
-x86Instructions.forEach(function (instruction) {
-	console.log(instruction);
-	/*
-	console.log("Mnemonic: ", instruction.mnemonic);
-	console.log("Short desc: ", instruction.shortDesc);
-	console.log("Long desc: ", instruction.longDesc);
-	*/
-})
-
-var x86Add = x86AsmRef.getInstructionByMnemonoc("ADD");
-console.log("Mnemonic: ", x86Add.mnemonic);
-console.log("Short desc: ", x86Add.shortDesc);
-console.log("Long desc: ", x86Add.longDesc);
+x86AsmRef.getAllInstructions(function (err, instructions) {
+	console.dir(instructions);
+	instructions.forEach(function (instruction) {
+		console.log(instruction);
+	});
+});
